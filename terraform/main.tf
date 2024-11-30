f3e36a337261bfb84f9a090ebb12e6ca05df8600
Alice Satan <satan.alice.t@gmail.com>
2024-11-30 14:15:45 +0300

    Create VM in Hyper-V via Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,28 +18,28 @@ provider "hyperv" {
 resource "hyperv_vhd" "tf_test_vhd" {
   path     = var.hyperv_vhd_path
   vhd_type = "Dynamic"
-  size     = 10737418240
+  size     = 1024 * 1024 * 1024 * 10 # 10GB
 }
 
 resource "hyperv_machine_instance" "tf_test_2" {
-  name            = "tf_test_2"
-  dynamic_memory  = true
-  processor_count = 4
-  path            = var.hyperv_vm_path
+  name                 = "tf_test_2"
+  dynamic_memory       = true
+  processor_count      = 4
+  path                 = var.hyperv_vm_path
+  memory_startup_bytes = 1024 * 1024 * 1024 * 1 # 1GB
   vm_firmware {
-    enable_secure_boot   = "Off"
-#    secure_boot_template = ""
+    enable_secure_boot = "Off"
   }
+
   hard_disk_drives {
     controller_type     = "Scsi"
     controller_location = 0
     controller_number   = 0
     path                = hyperv_vhd.tf_test_vhd.path
   }
-#  dvd_drives {
-#    controller_location = 1
-#    controller_number   = 2
-#    resource_pool_name  = "DVDs"
-#    #    path                = var.iso_oracle_linux_8
-#  }
+  dvd_drives {
+    controller_location = 1
+    controller_number   = 0
+    path                = var.iso_oracle_linux_8
+  }
 }
